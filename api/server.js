a35090bd4ea231ca6e3975768d576478dfa02d81
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-09-08 13:40:56 +0600

    Updated

--- api/server.js
+++ api/server.js
@@ -1,22 +1,52 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-const projectRoutes = require('../Routes/Projects');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// connect mongoose
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("Data base is connected"))
-    .catch(err => console.log(err.message))
 
-// Use project routes
-app.use('/projects', projectRoutes);
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.moc7rbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+
+    // collections
+    const projectCollection = client.db('test').collection('projects');
+
+app.get('/projects', async(req, res) => {
+    const result = await projectCollection.find().toArray();
+    res.send(result);
+})
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
 
 app.get('/', (req, res) => {
     res.send('This is my portfolio server')
